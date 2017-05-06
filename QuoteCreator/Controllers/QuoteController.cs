67e1cbabf1c0d375c6d5533e143c2828126d7260
DhruvSuthar <dhruvsuthar17@live.com>
2017-05-06 07:09:43 +0530

    Added quoteController

---
+++ QuoteCreator/Controllers/QuoteController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace QuoteCreator.Controllers
+{
+    public class QuoteController : ApiController
+    {
+        // GET api/<controller>
+        [Route("api/CreateQuote")]
+        public void Get()
+        {
+
+        }
+    }
+}
